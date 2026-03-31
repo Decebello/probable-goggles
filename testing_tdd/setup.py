89fda8f49d43d97c9cd322afbb2976a22592f40e
Victor <shulgins1978@gmail.com>
2026-04-01 04:22:34 +0800

    change setup.py

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.0.1",
     description="Калькулятор НДФЛ по прогрессивной шкале РФ (2025)",
     long_description=open("README.md", encoding="utf-8").read(),
-    url=,
+    url="https://github.com/Decebello/probable-goggles/tree/feature/ndfl-calculator200489",
     long_description_content_type = "text/markdown",
     package_dir = {"": "src"},
     packages = find_packages(where="src"),
